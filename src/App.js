f7bbb1f9fae08e5122ccd7059b6e67d182356c8e
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-04-23 17:13:34 +0530

    Added source code of booklist frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {ToastContainer} from "react-toastify"
+import { BrowserRouter, Route, Routes, json, useNavigate } from "react-router-dom"
+import Register from './components/Register';
+import Login from './components/Login';
+import Booklist from './components/Booklist';
+import AddBook from './components/AddBook';
+import { useEffect } from 'react';
+
+const Routing = ()=>{
+  const navigate = useNavigate()
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    if(!token){
+      return navigate("/")
+    }
+  },[])
+  return <Routes>
+  <Route path='/' element={<Register />} />
+  <Route path='/login' element={<Login />} />
+  <Route path='/booklist' element={<Booklist/>} />
+  <Route path='/addbook' element={<AddBook/>} />
+</Routes>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routing/>
+      <ToastContainer/>
+    </BrowserRouter>
+
   );
 }
 
